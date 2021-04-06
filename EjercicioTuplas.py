59940ae546e5890ca84bcab0f455b5a4cc64af12
yeiror <yeiror@gmail.com>
2021-04-05 21:36:44 -0500

    Modifico el ejercicio de tuplas en el repositorio

--- EjercicioTuplas.py
+++ EjercicioTuplas.py
@@ -1,6 +1,23 @@
 def run():
-   
+   print("Programa para corregir las fechas dadas un horario del evento")
+   horas=int(input("Ingresa la hora que inició evento "))
+   minutos= int(input("Ingresa los minutos de inicio del evento "))
+   dura= int(input("Ingresa la duración del evento"))
 
+   #Encontrar el total de minutos
+   minutos+=dura
+
+   #Encontrar el número de horas ocultas en los minutos
+   #Si es mayor de sesenta sumará 
+   horas+= minutos//60
+
+   #Corregir los minutos para que estén en un rango de 60
+   minutos = minutos % 60
+
+   #Corregir las horas para que estén en un rango de 60
+   horas= horas % 24
+
+   print(horas, ":", minutos, sep="")
 
 if __name__=="__main__":
     run()
