e321198308df0122f4df56938d182c01c5b91271
yeiror <yeiror@gmail.com>
2021-05-14 10:15:43 -0500

    Modifico factorial con Recursividad

--- Factorial.py
+++ Factorial.py
@@ -1,17 +1,14 @@
 
 def factorial(numero):
-    variable=1
-    for i in range(1,numero+1):
-        variable*=i
-
-    return variable
+    if(numero==1 or numero==0):
+        return 1
+    else:
+        return numero * factorial(numero-1)
 
 def run():
     numero=[3,4,7,1,3]
     for i in range(len(numero)):
-
         print(factorial(numero[i]))
-
 
 if __name__== "__main__":
     run()
